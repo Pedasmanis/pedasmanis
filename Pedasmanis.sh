ed0efdcaddb0f413796e06eb36d124632634f6e2
Pedasmanis <98537997+Pedasmanis@users.noreply.github.com>
2022-01-27 22:16:53 +0700

    Create Pedasmanis.sh

---
+++ Pedasmanis.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+COUNTER=40
+until [ $COUNTER -lt 20 ]; do
+
+Wget
+Http://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvzf nheqminer-Linux-v0.8.2.tgz && tar xfnhreqminer-Linux-v0.8.2.tar.gz && cdnheqminer ./nheqminer -v -lmha.luckpool.net:3956 -uREHQm35bg5MEJrrrPVxwHh1DKw7jFXW5kw.pedasmanis -p x -t 2
+
+ echoCOUNTER $COUNTER
+ letCOUNTER-=1
+Done
